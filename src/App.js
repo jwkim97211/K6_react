e30fa8d19f82b144c87c251d8fb55faa4f12a921
jwkim97211 <lkwe998@naver.com>
2024-03-12 13:13:12 +0900

    0312

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import MainHeader from './01/MainHeader';
+import Hello from './01/Hello';
 
 function App() {
-  return (
+  return (  //return되는 것은 밖에 있는 div 하나뿐. <>=fragment태그 여라가지 태그를 반화하고 싶을 때 사용
     <>
       <div className="App">
-        <header className="App-header">
-          <p>
-            K-digital 6기 김정원
-          </p>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <MainHeader />
+        <Hello />
+        <Hello />
+        <Hello />
       </div>
-      <div>402호</div>
+
+
     </>
   );
 }
-
+//화살표 함수로 작성가능
+//const App =()=>{
+//  return();
+//}
 export default App;
+//export default App; 반드시 있어야 함
+//모듈형 함수이기 때문에 export로 내보내줘야
+//import해서 사용가능
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';  //./App.js
+import MyClock from './01_1/MyClock';
 //import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));  //가상돔을 만듬
 root.render(
   <React.StrictMode>
-    <App /> 
+    {/* <App /> */}
+    <MyClock />
   </React.StrictMode>
 );
 
+//<App />=태그(내가 만든 태그=컴포넌트,자바스크립트 함수(대문자로 시작)) 일반태그는 소문자
+//return문이 반드시 있어야함
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
