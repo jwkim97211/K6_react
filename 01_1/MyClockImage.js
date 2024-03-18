045e873f06209d9bfe07664b4232b94dfcc66a10
jwkim97211 <160681245+jwkim97211@users.noreply.github.com>
2024-03-18 23:31:21 +0900

    Delete 01_1 directory

--- 01_1/MyClockImage.js
+++
@@ -1,9 +0,0 @@
-import logo from '../logo.svg';
-
-function MyClockImage() {
-    return (
-        <span style={{ display: "flex", justifyContent: "center" }}><img src={logo} className="App-logo" alt="logo" />
-        </span>
-    )
-}
-export default MyClockImage
