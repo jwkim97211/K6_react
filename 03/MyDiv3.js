cc23885edabc4d5cc4b3c78e9599a57dab84e50d
jwkim97211 <160681245+jwkim97211@users.noreply.github.com>
2024-03-18 23:31:36 +0900

    Delete 03 directory

--- 03/MyDiv3.js
+++
@@ -1,9 +0,0 @@
-export default function MyDiv3(probs) {
-  return (
-    <div className="flex justify-center items-start bg-gray-500 w-4/5 h-4/5 text-white">
-    <p className="flex justify-start w-4/5 font-bold text-2xl my-3">
-    {`${probs.dn1} > ${probs.dn2} > ${probs.dn3}`}
-    </p>
-</div>
-  )
-}
