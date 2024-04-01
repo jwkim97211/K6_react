103b3059996ac19708ab3602f71d3b7135cb0adf
jwkim97211 <lkwe998@naver.com>
2024-04-01 17:59:19 +0900

    0401

--- src/14/Frsct.js
+++ src/14/Frsct.js
@@ -46,6 +46,7 @@ export default function Frsct() {
     )
 }
 
-// 초단기
+//초단기
 //https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=EvGbclvzO9v1U1l5eMgKpF3H4YVgRbWX24VTGEPhG66K04BPc73tLUDzWsqfOak4WRpRWEwupZYSiGPcnmAPKw%3D%3D&pageNo=1&numOfRows=1000&dataType=json&base_date=20240401&base_time=0630&nx=55&ny=127
+//단기
 //https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=EvGbclvzO9v1U1l5eMgKpF3H4YVgRbWX24VTGEPhG66K04BPc73tLUDzWsqfOak4WRpRWEwupZYSiGPcnmAPKw%3D%3D&pageNo=1&numOfRows=1000&dataType=json&base_date=20240401&base_time=0500&nx=55&ny=127
