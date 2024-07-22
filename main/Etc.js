eb40b55fd07ebc3af9d13a2f6a6858c47bfbe2f8
jwkim97211 <160681245+jwkim97211@users.noreply.github.com>
2024-07-22 11:54:43 +0900

    Delete main directory

--- main/Etc.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Etc() {
-  return (
-    <div className='bg-slate-100 bg-opacity-80 flex flex-col w-1/6 rounded-xl items-center justify-center'>
-      asd
-    </div>
-  )
-}
